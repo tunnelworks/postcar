d171dcee9a826478ad125afa0ed35bda602f8de9
Dan Lindholm <git@dan.mk>
2024-10-18 16:32:11 +0200

    update scope of test constants

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def package() -> str:
     return "tests.example.migrations"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def invalid_package() -> str:
     return "tests.example.invalid"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def migration_name() -> str:
     return "0001_init"
