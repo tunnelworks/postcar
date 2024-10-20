226e8a21a835e96cd81b8805ff9550e1a48a9a4b
Dan Lindholm <git@dan.mk>
2024-10-20 19:40:11 +0200

    expose version string

--- src/postcar/__init__.py
+++ src/postcar/__init__.py
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: 2024-present tunnelworks <git@tunnelworks.org>
 #
 # SPDX-License-Identifier: Apache-2.0
+from postcar.__about__ import __version__
 from postcar.db import Migration
 
 
-__all__ = ("Migration",)
+__all__ = (
+    "__version__",
+    "Migration",
+)
