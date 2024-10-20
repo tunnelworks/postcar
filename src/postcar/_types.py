a8741ba6e1997ecf21d6dc62f6d4a0b3623b538f
Dan Lindholm <git@dan.mk>
2024-10-20 19:15:21 +0200

    add types for internal migrations

--- src/postcar/_types.py
+++ src/postcar/_types.py
@@ -4,6 +4,7 @@ import typing as t
 if t.TYPE_CHECKING:
     import psycopg
     from psycopg.rows import TupleRow
+    from typing_extensions import Self
 
     Connection = psycopg.AsyncConnection[TupleRow]
 
@@ -16,3 +17,21 @@ class Module(t.NamedTuple):
         if self.package is None:
             return self.name
         return f"{self.package}:{self.name}"
+
+
+class Version(t.NamedTuple):
+    major: int
+    minor: int
+
+    def __str__(self) -> str:
+        return ".".join(map(str, self))
+
+    @classmethod
+    def fromstring(cls, version: str) -> "Self":
+        major, minor, *_ = version.split(".")
+        return cls(major=int(major), minor=int(minor))
+
+
+class InternalMigration(t.NamedTuple):
+    version: Version
+    query: str
